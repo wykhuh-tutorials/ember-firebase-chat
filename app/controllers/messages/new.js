161a06a4b8d9f68f47084f5c337ac3b1ccc7563d
wykhuh <wykhuh@users.noreply.github.com>
2015-05-01 08:34:49 -0700

    initial commmit

---
+++ app/controllers/messages/new.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    addMessage: function() {
+      var newMessage = this.store.createRecord('message', {
+        name: this.get('name'),
+        body: this.get('body')
+      });
+
+
+      newMessage.save();
+      this.setProperties({
+        name: '',
+        body: ''
+      });
+    }
+  }
+});
---
+++ app/router.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+var Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+export default Router.map(function() {
+  this.route('about');
+  this.resource('messages', function() {
+    this.route('new');
+  });
+});
