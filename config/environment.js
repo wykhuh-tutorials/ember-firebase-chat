5d2bf9ec3bea1c3da4f0e60e6d050aa9666c52b7
wykhuh <wykhuh@users.noreply.github.com>
2015-05-01 20:50:04 -0700

    change base url

--- config/environment.js
+++ config/environment.js
@@ -6,7 +6,7 @@ module.exports = function(environment) {
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' wss://*.firebaseio.com" },
     firebase: 'https://ember-fire-chat1.firebaseio.com/',
-    baseURL: '/',
+    baseURL: '/ember-firebase-chat',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
